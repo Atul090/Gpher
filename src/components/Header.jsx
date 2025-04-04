328977999d369c07c5c9a276c3658e7c3624ceae
Atul Deep Singh <999.atulsingh@gmail.com>
2025-04-05 00:14:04 +0530

    Header component blueprint completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import { HiMiniBars3BottomRight, HiOutlineEllipsisVertical } from 'react-icons/hi2';
 
 const Header = () => {
     const [categories, setCategories] = useState([]);
@@ -16,11 +17,35 @@ const Header = () => {
                         GIPHY
                     </h1>
                 </Link>
+                <div className='font-bold text-md flex gap-2 items-center'>
                 {/* render categories */}
                 <Link className='px-4 py-1 hover:gradient border-b-4 hidden lg:block'>
                     Reactions
                 </Link>
+                <button onClick={()=>setShowCategories(!showCategories)}>
+                    <HiOutlineEllipsisVertical 
+                        size={35}
+                        className={`py-0.5 hover:gradient ${showCategories ? "gradient":""} border-b-4 hidden lg:block`}>
+                    </HiOutlineEllipsisVertical>
+                </button>
+                <div className='h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded'>
+                    <Link to="/favourites">Favourite GIFs</Link>
+                        <div>
+                            <button>
+                                <HiMiniBars3BottomRight className='text-sky-400 block lg:hidden' size={30}/>
+                            </button>
+                        </div> 
+                </div>
+                </div>
+                {showCategories && <div className='absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20'>
+                    <span>Categories</span>
+                    <hr />
+                    <div>
+                        <Link className='font-bold'>Reactions</Link>
+                    </div>
+                </div>}
             </div>
+            {/*  search  */}
         </nav>
     )       
 }
